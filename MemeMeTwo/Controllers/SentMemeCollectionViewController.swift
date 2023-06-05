9f6e339eefd8995c31d417ec4c873503bf140ff8
Paul Lewallen <paullewallencom@users.noreply.github.com>
2023-06-04 21:13:01 -0700

    Commented SentMemeCollectionViewController
    Commented SentMemeCollectionViewController.

--- MemeMeTwo/Controllers/SentMemeCollectionViewController.swift
+++ MemeMeTwo/Controllers/SentMemeCollectionViewController.swift
@@ -9,5 +9,11 @@
 import UIKit
 
 class SentMemeCollectionViewController {
+    
+    // MARK: Properties and Outlets
+    
+    // MARK: UICollectionView Data Source
+    
+    // MARK: UICollectionView Delegates
     // TODO: Add stuff
 }
