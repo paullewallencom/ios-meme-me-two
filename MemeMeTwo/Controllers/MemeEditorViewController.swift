7bae949a3d14831be46800c831bbacd90f8e3800
Paul Lewallen <paullewallencom@users.noreply.github.com>
2023-06-04 21:04:29 -0700

    Commented MemeEditorViewController
    Commented MemeEditorViewController.

--- MemeMeTwo/Controllers/MemeEditorViewController.swift
+++ MemeMeTwo/Controllers/MemeEditorViewController.swift
@@ -10,6 +10,32 @@ import UIKit
 
 class MemeEditorViewController {
     
+    // MARK: IBOutlets
+    
+    // MARK: Properties
+    
+    // MARK: LifeCycle Methods
+    
+    // MARK: UIImagePickerController Functions
+    
+    // MARK: UIImagePickerController Delegates
+    
+    // MARK: Notification Functions
+    
+    // MARK: Keyboard Related Methods and Delegates
+    
+    // MARK: Generating Meme Objects
+    
+    // MARK: Top Bar Button Actions
+    
+    // MARK: Helper Functions
+    
+    // MARK: Pop Animation
     
     // TODO: Add stuff
 }
+
+extension MemeEditorViewController {}
+
+extension MemeEditorViewController {}
+
