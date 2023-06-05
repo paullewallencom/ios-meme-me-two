02ebc2bcb77ede4d5a203d343c0fc5590057cc6c
Paul Lewallen <paullewallencom@users.noreply.github.com>
2023-06-04 21:16:08 -0700

    Commented SentMemeTableViewController
    Commented SentMemeTableViewController.

--- MemeMeTwo/Controllers/SentMemeTableViewController.swift
+++ MemeMeTwo/Controllers/SentMemeTableViewController.swift
@@ -9,5 +9,15 @@
 import UIKit
 
 class SentMemeTableViewController {
+    
+    // MARK: Properties and Outlets
+    
+    // MARK: LifeCycle Methods
+    
+    // MARK: Table View Data Source
+    
+    // MARK: Table View Delegates
+    
+    
     // TODO: Add stuff
 }
