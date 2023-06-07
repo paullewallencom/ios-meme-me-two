7893b5a354e560f0902193ddb358613dbe7f0f0f
Paul Lewallen <paullewallencom@users.noreply.github.com>
2023-06-06 19:28:47 -0700

    Updated FontTableViewController
    Updated FontTableViewController.

--- MemeMeTwo/Controllers/FontTableViewController.swift
+++ MemeMeTwo/Controllers/FontTableViewController.swift
@@ -8,23 +8,43 @@
 
 import UIKit
 
-class FontTableViewController {
+class FontTableViewController  : UITableViewController {
     
     // MARK: Properties
     let fontData = AppModel.fontAvailable
     
     // MARK: LifeCycle Methods
-    override func viewWillAppear () {}
+    override func viewWillAppear (_ animated : Bool) {
+        self.navigationController?.isNavigationBarHidden = false
+    }
     
     // MARK: Table View Data Source
-    override func numberOfSections () {}
+    override func numberOfSections (in tableView : UITableView) -> Int {
+        return 1
+    }
     
-    override func tableView () {}
+    override func tableView (_ tableView : UITableView, numberOfRowsInSection section : Int) -> Int {
+        return fontData.count
+    }
     
     // MARK: Table View Delegates
-    override func tableView () {}
+    override func tableView (_ tableView : UITableView, cellForRowAt indexPath : IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell (withIdentifier : AppModel.fontCellReuseIdentifier, for : indexPath)
+        
+        cell.textLabel?.text = fontData[indexPath.row]
+        cell.textLabel?.font = UIFont(name : fontData[indexPath.row], size : 20)
+        
+        if indexPath.row == AppModel.currentFontIndex {
+            cell.accessoryType = .checkmark
+        } else {
+            cell.accessoryType = .none
+        }
+        return cell
+    }
     
-    override func tableView () {}
-    
-    // TODO: Add stuff
+    override func tableView (_ tableView : UITableView, didSelectRowAt indexPath : IndexPath) {
+        AppModel.currentFontIndex = indexPath.row
+        AppModel.selectedFont = fontData[indexPath.row]
+        tableView.reloadData ()
+    }
 }
